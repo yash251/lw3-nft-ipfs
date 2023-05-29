787776580a94582015c6dde66f41fe71285f00e3
Yash Jagtap <jagtapyash2512@gmail.com>
2023-05-29 17:46:31 +0530

    all state vars

---
+++ smart-contracts/contracts/LW3Punks.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract LW3Punks is ERC721Enumerable, Ownable {
+    using Strings for uint256;
+    /**
+        * @dev _baseTokenURI for computing {tokenURI}. If set, the resulting URI for each
+        * token will be the concatenation of the `baseURI` and the `tokenId`.
+    */
+    string _baseTokenURI;
+
+    //  _price is the price of one LW3Punks NFT
+    uint256 public _price = 0.01 ether;
+
+    // _paused is used to pause the contract in case of an emergency
+    bool public _paused;
+
+    // max number of LW3Punks
+    uint256 public maxTokenIds = 10;
+
+    // total number of tokenIds minted
+    uint256 public tokenIds;
+}
