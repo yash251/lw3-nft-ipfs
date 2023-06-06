6a0e83294f72ada74848ffb9c272b26c78e66c36
Yash Jagtap <jagtapyash2512@gmail.com>
2023-06-06 13:25:33 +0530

    LW3 to Yash

--- smart-contracts/contracts/LW3Punks.sol
+++ smart-contracts/contracts/LW3Punks.sol
@@ -13,13 +13,13 @@ contract LW3Punks is ERC721Enumerable, Ownable {
     */
     string _baseTokenURI;
 
-    //  _price is the price of one LW3Punks NFT
+    //  _price is the price of one YashPunks NFT
     uint256 public _price = 0.01 ether;
 
     // _paused is used to pause the contract in case of an emergency
     bool public _paused;
 
-    // max number of LW3Punks
+    // max number of YashPunks
     uint256 public maxTokenIds = 10;
 
     // total number of tokenIds minted
@@ -43,7 +43,7 @@ contract LW3Punks is ERC721Enumerable, Ownable {
     * @dev mint allows an user to mint 1 NFT per transaction.
     */
     function mint() public payable onlyWhenNotPaused {
-        require(tokenIds < maxTokenIds, "Exceed maximum LW3Punks supply");
+        require(tokenIds < maxTokenIds, "Exceed maximum YashPunks supply");
         require(msg.value >= _price, "Ether sent is not correct");
         tokenIds += 1;
         _safeMint(msg.sender, tokenIds);
