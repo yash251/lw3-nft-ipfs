a7bd4abdad9fd6dff8f1e762a4ba0559366f5470
Yash Jagtap <jagtapyash2512@gmail.com>
2023-06-14 23:02:49 +0530

    all imports

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,4 +1,9 @@
-import Image from 'next/image'
+import { Contract, providers, utils } from "ethers";
+import Head from "next/head";
+import React, { useEffect, useRef, useState } from "react";
+import Web3Modal from "web3modal";
+import { abi, NFT_CONTRACT_ADDRESS } from "../constants";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
