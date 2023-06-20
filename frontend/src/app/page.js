d839eabbba464774837070a4161d011b4919c8ce
Yash Jagtap <jagtapyash2512@gmail.com>
2023-06-20 19:36:22 +0530

    useEffect

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -109,6 +109,31 @@ export default function Home() {
     }
     return web3Provider;
   };
+
+  // useEffects are used to react to changes in state of the website
+  // The array at the end of function call represents what state changes will trigger this effect
+  // In this case, whenever the value of `walletConnected` changes - this effect will be called
+  useEffect(() => {
+    // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
+    if (!walletConnected) {
+      // Assign the Web3Modal class to the reference object by setting it's `current` value
+      // The `current` value is persisted throughout as long as this page is open
+      web3ModalRef.current = new Web3Modal({
+        network: "mumbai",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+
+      connectWallet();
+
+      getTokenIdsMinted();
+
+      // set an interval to get the number of token Ids minted every 5 seconds
+      setInterval(async function () {
+        await getTokenIdsMinted();
+      }, 5 * 1000);
+    }
+  }, [walletConnected]);
   
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
